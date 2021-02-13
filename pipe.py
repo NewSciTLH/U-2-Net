c2c84402adb334b4f6bb855ac1d6cc27e9710a00
Eric Dolores <ericd@newsci.ai>
2021-02-13 21:00:25 +0000

    tables added

--- pipe.py
+++ pipe.py
@@ -17,9 +17,11 @@ else:
     storage_client = storage.Client()
     
 print('Downloading weights...')
-utils.downloadBlob('model_staging','eyes/ResNet50_Models/cat_model_epoch_015.pth', 'checkpoints/cat_model_epoch_015.pth')
-utils.downloadBlob('model_staging','eyes/ResNet50_Models/dog_model_epoch_015.pth', 'checkpoints/dog_model_epoch_015.pth')
-utils.downloadBlob('model_staging','eyes/ResNet50_Models/human_model_epoch_015.pth', 'checkpoints/human_model_epoch_015.pth')
+
+if not os.path.isfile('checkpoints/cat_model_epoch_015.pth'):
+    utils.downloadBlob('model_staging','eyes/ResNet50_Models/cat_model_epoch_015.pth', 'checkpoints/cat_model_epoch_015.pth')
+    utils.downloadBlob('model_staging','eyes/ResNet50_Models/dog_model_epoch_015.pth', 'checkpoints/dog_model_epoch_015.pth')
+    utils.downloadBlob('model_staging','eyes/ResNet50_Models/human_model_epoch_015.pth', 'checkpoints/human_model_epoch_015.pth')
 print('done')
 
 
@@ -31,7 +33,7 @@ FROM reconciliation_input
 """
     results = query_client.query(QUERY).result()
     print(f"We have {results.total_rows} folders of orders in the directories socks, koozies and stickers")
-     key, key_m, source_blob_name, source_blob_name_m, bucket, 'human'
+    key, key_m, source_blob_name, source_blob_name_m, bucket, 'human'
     to_list = [ {"key":item['key'],
                  "key_m":item['key_m'],
                  "bucket": item['simple_crop'].split('/')[0],
